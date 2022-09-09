1cac5d7335f829e3a1f19240bd0e188374d6dcab
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-09-09 12:57:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.jpeg";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}
+        className="App-logo
+        img-fluid"
+        alt="logo" />
+      Hello world.
+    </header>
+    </div>
+  );
+}
+
+
